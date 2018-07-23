35f9e950cbc6eba2080f05df3df3cf72ddb0b7a9
Megapunkdev <32435920+Megapunkdev@users.noreply.github.com>
2018-07-23 18:51:42 +0200

    Fixed dropdown menu/navbar

--- script/script.js
+++ script/script.js
@@ -1,13 +1,17 @@
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+let dropdown = document.querySelector('.dropdown');
+let dropdowncontent = document.querySelector('.dropdown-content');
+let dropbtn = document.querySelector('.dropbtn');
+
+dropdown.addEventListener('mouseover', () => {
+  dropDownToggle();
+})
+
+dropdown.addEventListener('mouseout', () => {
+  dropDownToggle();
+})
+
+let dropDownToggle = () => {
+  dropdowncontent.classList.toggle('show');
 }
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn')) {
-    var myDropdown = document.getElementById("myDropdown");
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
-      }
-  }
-}
+
